b85a893e6e4c228bd1e0bffd2cf969ca3a5f0583
Rafael Jardim <rafaelmjardim@gmail.com>
2024-03-31 22:41:49 -0300

    update port 3333

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     return res.json({message: 'Bem-vindo'})
 })
 
-app.listen(443, () => {
+app.listen(3333, () => {
     console.log(`Servidor rodando na porta 3333`);
 })
